b7c57b84c03db738768c90e927761e432610d063
Grant Gurvis <grangurv@amazon.com>
2025-01-14 14:03:39 -0800

    fix test

--- crates/fig_test_utils/src/server.rs
+++ crates/fig_test_utils/src/server.rs
@@ -4,7 +4,6 @@ use std::net::SocketAddr;
 use std::pin::Pin;
 
 use bytes::Bytes;
-pub use http::Method;
 use http_body_util::Full;
 use hyper::body::Incoming;
 use hyper::service::Service;
@@ -20,34 +19,10 @@ use tokio::net::{
 use tokio::select;
 use tokio_util::sync::CancellationToken;
 
-/// A local HTTP server that can be used for returning mock responses to HTTP requests.
-///
-/// # Examples
-///
-/// ```rust
-/// # use fig_test_utils::server::*;
-/// # async fn run() -> Result<(), reqwest::Error> {
-/// // Hosting a local server that responds to GET requests for "/my-file" with the
-/// // body "some text".
-/// let test_path = String::from("/my-file");
-/// let mock_body = String::from("some text");
-/// let test_server_addr = TestServer::new()
-///     .await
-///     .with_mock_response(Method::GET, test_path.clone(), mock_body.clone())
-///     .spawn_listener();
-///
-/// let body = reqwest::get(format!("http://{}{}", &test_server_addr, &test_path))
-///     .await?
-///     .text()
-///     .await?;
-///
-/// assert_eq!(body, mock_body);
-/// # Ok(())
-/// # }
-/// ```
 #[derive(Debug)]
 pub struct TestServer {
     listener: TcpListener,
+    cancellation_token: Option<CancellationToken>,
     mock_responses: HashMap<(http::Method, String), String>,
 }
 
@@ -58,6 +33,7 @@ impl TestServer {
             .expect("failed to bind socket to local host");
         Self {
             listener,
+            cancellation_token: None,
             mock_responses: HashMap::default(),
         }
     }
@@ -67,14 +43,14 @@ impl TestServer {
         self
     }
 
-    /// Spawns a new task for accepting requests, returning the address of the listening socket.
-    pub fn spawn_listener(self) -> TestAddress {
-        let address = self
+    pub fn spawn_listener(mut self) -> SocketAddr {
+        let addr = self
             .listener
             .local_addr()
             .expect("listener should be bound to an address");
-        let cancellation_token = CancellationToken::new();
-        let token_clone = cancellation_token.clone();
+        let token = CancellationToken::new();
+        let token_clone = token.clone();
+        self.cancellation_token = Some(token);
         tokio::task::spawn(async move {
             loop {
                 select! {
@@ -88,10 +64,7 @@ impl TestServer {
                 }
             }
         });
-        TestAddress {
-            address,
-            cancellation_token,
-        }
+        addr
     }
 
     async fn handle_request(&self, stream: TcpStream) {
@@ -103,27 +76,11 @@ impl TestServer {
     }
 }
 
-#[derive(Debug)]
-pub struct TestAddress {
-    address: SocketAddr,
-    cancellation_token: CancellationToken,
-}
-
-impl TestAddress {
-    pub fn address(&self) -> SocketAddr {
-        self.address
-    }
-}
-
-impl std::fmt::Display for TestAddress {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.address)
-    }
-}
-
-impl Drop for TestAddress {
+impl Drop for TestServer {
     fn drop(&mut self) {
-        self.cancellation_token.cancel();
+        if let Some(token) = &self.cancellation_token {
+            token.cancel();
+        }
     }
 }
 
@@ -141,36 +98,5 @@ impl Service<Request<Incoming>> for TestServer {
         let path = req.uri().path().to_string();
         let result = self.mock_responses.get(&(method, path)).unwrap().clone();
         Box::pin(async move { Ok(Response::builder().status(200).body(result.into()).unwrap()) })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn test_server_mock_and_drop() {
-        let test_path = String::from("/test-path");
-        let test_response = String::from("test body");
-        let test_server_addr = TestServer::new()
-            .await
-            .with_mock_response(Method::GET, test_path.clone(), test_response.clone())
-            .spawn_listener();
-
-        let response = reqwest::get(format!("http://{}{}", &test_server_addr, &test_path))
-            .await
-            .unwrap()
-            .text()
-            .await
-            .unwrap();
-        assert_eq!(response, test_response);
-
-        // Test that dropping TestAddress stops the server
-        let addr = test_server_addr.address();
-        std::mem::drop(test_server_addr);
-        // wait for the task to complete
-        tokio::time::sleep(std::time::Duration::from_millis(10)).await;
-        let response = reqwest::get(format!("http://{}{}", &addr, &test_path)).await;
-        assert!(response.is_err());
     }
 }
